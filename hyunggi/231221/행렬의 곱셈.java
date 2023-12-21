976f2dbd6aec1adbb21fa8ea0fe41857c173e3eb
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2023-12-21 21:51:30 +0900

    Hyunggi - 231221 문제풀이 (#3)
    * feat: 거리두기 확인하기 upload
    
    * feat: 행렬의 곱셈 upload

---
+++ hyunggi/231221/행렬의 곱셈.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        final int newRowNum = arr1.length;
+        final int newColNum = arr2[0].length;
+
+        final int[][] answer = new int[newRowNum][newColNum];
+
+        //행렬의 곱셈
+        for (int row = 0; row < newRowNum; row++) {
+            for (int col = 0; col < newColNum; col++) {
+                int tempValue = 0;
+
+                //arr1 col만큼, arr2 row만큼
+                for (int idx = 0; idx < arr1[0].length; idx++) {
+                    tempValue += arr1[row][idx] * arr2[idx][col];
+                }
+
+                answer[row][col] = tempValue;
+            }
+        }
+
+        return answer;
+    }
+}
