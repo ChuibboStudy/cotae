2c6c5254007548737074254df18653d27007a18c
HyunggiPark <phg9587@naver.com>
2023-12-22 09:58:52 +0900

    feat: 이상한 문자 만들기 upload

---
+++ hyunggi/231222/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(final String s) {
+        String answer = "";
+
+        int numCount = 1;
+
+        for (int idx = 0; idx < s.length(); idx++) {
+            final char ch = s.charAt(idx);
+
+            if (ch == ' ') {
+                answer += " ";
+                numCount = 1;
+                continue;
+            }
+
+            if (numCount % 2 == 1) {
+                answer += Character.toUpperCase(ch);
+            } else {
+                answer += Character.toLowerCase(ch);
+            }
+            numCount++;
+        }
+
+        return answer;
+    }
+}
