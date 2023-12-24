b4eaaabb5fe9f75adcf38f55c46b24442d0143f7
HyunggiPark <phg9587@naver.com>
2023-12-24 13:23:01 +0900

    feat: 문자열 내 p와 y의 개수 upload

---
+++ hyunggi/231224/문자열 내 p와 y의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(final String s) {
+        int pCount = 0;
+        int yCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            final char ch = s.charAt(i);
+
+            if (Character.toUpperCase(ch) == 'P') {
+                pCount++;
+            } else if (Character.toUpperCase(ch) == 'Y') {
+                yCount++;
+            }
+        }
+
+        return pCount == yCount;
+    }
+}
