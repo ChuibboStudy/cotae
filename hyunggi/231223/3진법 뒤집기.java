2ade7af68afb0e8c7bc99e7c4e439550b53a7d60
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2023-12-24 13:20:45 +0900

    Hyunggi - 231222 문제풀이 (#7)
    * feat: 3진법 뒤집기 upload
    
    * feat: 이진 변환 반복하기 upload

---
+++ hyunggi/231223/3진법 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        final StringBuilder tempStr = new StringBuilder();
+
+        while (n > 0) {
+            tempStr.append(n % 3);
+            n /= 3;
+        }
+
+        return Integer.valueOf(tempStr.toString(), 3);
+    }
+}
---
+++ hyunggi/231223/이진 변환 반복하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String s) {
+        int zeroCount = 0;
+        int convertCount = 0;
+
+        while (!s.equals("1")) {
+            final int beforeLength = s.length();
+            final int afterLength = s.replace("0", "").length();
+
+            zeroCount += beforeLength - afterLength;
+
+            convertCount++;
+
+            s = Integer.toBinaryString(afterLength);
+        }
+
+        return new int[]{convertCount, zeroCount};
+    }
+}
