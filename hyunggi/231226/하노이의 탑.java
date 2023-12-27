c7c49b08bb4a981bcf90851193438a9887f90e6f
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2023-12-27 21:41:49 +0900

    Hyunggi - 231226 문제풀이 (#11)
    * feat: 쿼드 압축 후 개수 세기 upload
    
    * feat: 하노이의 탑 upload

---
+++ hyunggi/231226/하노이의 탑.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    private List<int[]> answer;
+
+    public int[][] solution(final int totalNum) {
+        answer = new ArrayList<>();
+
+        hanoi(totalNum, 1, 2, 3);
+
+        return answer.toArray(int[][]::new);
+    }
+
+    private void hanoi(final int totalNum, final int from, final int by, final int to) {
+        if (totalNum == 1) {
+            answer.add(new int[]{from, to});
+            return;
+        }
+
+        //totalNum - 1개를 from에서 by로 옮긴다.
+        hanoi(totalNum - 1, from, to, by);
+
+        //1개를 from에서 to로 옮긴다.
+        hanoi(1, from, by, to);
+
+        //totalNum - 1개를 by에서 to로 옮긴다.
+        hanoi(totalNum - 1, by, from, to);
+    }
+}
