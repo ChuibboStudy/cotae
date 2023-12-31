71e4742751f900e4db6cb9ffae2dc9d1b9f0a585
HyunggiPark <phg9587@naver.com>
2023-12-31 15:23:15 +0900

    feat: 문자열 내림차순 upload

---
+++ hyunggi/231231/문자열 내림차순.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public String solution(final String s) {
+        final char[] chars = s.toCharArray();
+
+        insertionSort(chars);
+
+        return String.valueOf(chars);
+    }
+
+    private void insertionSort(final char[] chars) {
+        //1번째부터 끝까지 돌면서 자기 자리를 찾는다.
+        for (int selectIdx = 1; selectIdx < chars.length; selectIdx++) {
+            final char selectValue = chars[selectIdx];
+            int diffIdx = selectIdx - 1;
+
+            //0번째까지 다 비교를 해서 자기 자리를 찾는다.
+            for (; diffIdx >= 0; diffIdx--) {
+                final char diffValue = chars[diffIdx];
+
+                //자기 자리를 찾을 때 까지 앞의 값들을 뒤로 복사한다.
+                if (diffValue >= selectValue) {
+                    break;
+                }
+
+                chars[diffIdx + 1] = diffValue;
+            }
+
+            chars[diffIdx + 1] = selectValue;
+        }
+    }
+}
