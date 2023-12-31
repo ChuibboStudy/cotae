7897006e8c9ea593bf483e3176caa28b4969b74f
HyunggiPark <phg9587@naver.com>
2023-12-31 15:22:13 +0900

    feat: H-Index upload

---
+++ hyunggi/231231/H-Index.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(final int[] citations) {
+        final int length = citations.length;
+        int answer = length;
+
+        Arrays.sort(citations);
+
+        for (int idx = 0; idx < citations.length; idx++) {
+            //현재 인용 개수
+            final int curCitation = citations[idx];
+
+            //H-index 이상 인용 개수
+            final int upperCount = length - idx;
+            //H-index 이하 인용 개수
+            final int lowerCount = idx;
+
+            //H-index 조건을 만족하는지 확인
+            if (answer <= curCitation
+                    && (lowerCount <= answer && answer <= upperCount)) {
+                break;
+            }
+
+            //H-index 조건을 만족하지 않으면 H-index 감소
+            answer--;
+        }
+
+        return answer;
+    }
+}
