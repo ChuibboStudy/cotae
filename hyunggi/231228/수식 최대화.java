c1d77a2df08efed2619fcadee21e116d58574c11
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-01-01 13:56:02 +0900

    Hyunggi - 231228 문제풀이 (#15)
    * feat: 카펫 upload
    
    * feat: 수식 최대화 upload
    
    * feat: 수식 최대화 리팩터링 upload

---
+++ hyunggi/231228/수식 최대화.java
@@ -0,0 +1,121 @@
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Solution {
+    List<List<String>> operatorPriorities = List.of(
+            List.of("+", "-", "*"),
+            List.of("+", "*", "-"),
+            List.of("-", "+", "*"),
+            List.of("-", "*", "+"),
+            List.of("*", "+", "-"),
+            List.of("*", "-", "+")
+    );
+
+    public long solution(final String expression) {
+        long answer = 0;
+
+        final String[] originNumbers = getOriginNumbers(expression);
+        final List<String> originOperator = getOriginOperator(expression);
+
+        for (final List<String> operatorPriority : operatorPriorities) {
+            final Deque<String> postfixQueue = calculatePostfixQueue(operatorPriority, originNumbers, originOperator);
+
+            answer = Math.max(answer, Math.abs(calculateAnswer(postfixQueue)));
+        }
+
+        return answer;
+    }
+
+    private String[] getOriginNumbers(final String expression) {
+        return expression.replace("-", ",")
+                .replace("*", ",")
+                .replace("+", ",")
+                .split(",");
+    }
+
+    private List<String> getOriginOperator(final String expression) {
+        final List<String> originOperators = new ArrayList<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            final char ch = expression.charAt(i);
+
+            if (ch == '-' || ch == '*' || ch == '+') {
+                originOperators.add(String.valueOf(ch));
+            }
+        }
+
+        return originOperators;
+    }
+
+    private Deque<String> calculatePostfixQueue(final List<String> operatorPriority, final String[] originNumbers, final List<String> originOperator) {
+        final Deque<String> postfixQueue = new ArrayDeque<>();
+        final Deque<String> operatorStack = new ArrayDeque<>();
+
+        //postfix로 만들기
+        for (int idx = 0; idx < originNumbers.length; idx++) {
+            //숫자 넣기
+            postfixQueue.addLast(originNumbers[idx]);
+
+            //연산자 넣기
+            if (idx < originOperator.size()) {
+                final String curOperator = originOperator.get(idx);
+
+                while (!operatorStack.isEmpty()) {
+                    final String peekOperator = operatorStack.peek();
+
+                    //현재 연산자가 우선순위가 더 높으면 그냥 push, 아니면 다 꺼내서 Queue에 넣기
+                    if (operatorPriority.indexOf(peekOperator) < operatorPriority.indexOf(curOperator)) {
+                        break;
+                    }
+
+                    postfixQueue.addLast(operatorStack.pop());
+                }
+
+                operatorStack.push(curOperator);
+            }
+        }
+
+        //남은 연산자 다 꺼내서 Queue에 넣기
+        while (!operatorStack.isEmpty()) {
+            postfixQueue.addLast(operatorStack.pop());
+        }
+
+        return postfixQueue;
+    }
+
+    private long calculateAnswer(final Deque<String> postfixQueue) {
+        final Deque<Long> calculateStack = new ArrayDeque<>();
+
+        while (!postfixQueue.isEmpty()) {
+            final String popped = postfixQueue.pop();
+
+            //연산자 아니면 Stack에 넣기
+            final List<String> operators = List.of("+", "-", "*");
+            if (!operators.contains(popped)) {
+                calculateStack.push(Long.valueOf(popped));
+                continue;
+            }
+
+            //연산자 맞으면 계산
+            final long value2 = calculateStack.pop();
+            final long value1 = calculateStack.pop();
+
+            switch (popped) {
+                case "+":
+                    calculateStack.push(value1 + value2);
+                    break;
+                case "-":
+                    calculateStack.push(value1 - value2);
+                    break;
+                case "*":
+                    calculateStack.push(value1 * value2);
+                    break;
+            }
+        }
+
+        return calculateStack.pop();
+    }
+}
---
+++ hyunggi/231228/카펫.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public int[] solution(final int brown, final int yellow) {
+        int[] answer = {};
+
+        //y 높이로 가능한 숫자들을 돌리면서, 총 개수를 구해야 한다.
+        //brown 개수 => 4 + 높이 * 2 + 너비 * 2
+
+        final double minWidth = Math.sqrt(yellow);
+        for (int width = yellow; width >= minWidth; width--) {
+            final double height = (double) yellow / width;
+
+            //정수가 아니면 넘어간다.
+            if (height % 1 != 0) {
+                continue;
+            }
+
+            final int intHeight = (int) height;
+            final int tempBrown = 4 + intHeight * 2 + width * 2;
+
+            if (tempBrown == brown) {
+                answer = new int[]{width + 2, intHeight + 2};
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
