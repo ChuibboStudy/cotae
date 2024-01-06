7e9585bc8cf60d7374698b867649c1f106b123e7
HyunggiPark <phg9587@naver.com>
2024-01-06 12:58:49 +0900

    feat: 중복된 문자 제거 upload

---
+++ hyunggi/240106/중복된 문자 제거.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String solution(final String my_string) {
+        final StringBuilder sb = new StringBuilder();
+        final Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            final char ch = my_string.charAt(i);
+
+            if (set.contains(ch)) {
+                continue;
+            }
+
+            set.add(ch);
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+}
