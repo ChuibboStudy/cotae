25fde700bcabd6097fc066c42654f6017c8ec854
HyunggiPark <phg9587@naver.com>
2024-01-15 15:15:35 +0900

    feat: 괄호 회전하기 upload

---
+++ hyunggi/240115/괄호 회전하기.java
@@ -0,0 +1,58 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int solution(final String s) {
+        int answer = 0;
+
+        final int length = s.length();
+
+        if (isOddLength(length)) {
+            return 0;
+        }
+
+        final String newStr = s + s;
+
+        //length만큼 반복 -> 왼쪽으로 이동
+        //매번 스택이 되는지 확인
+        for (int startIdx = 0; startIdx < s.length(); startIdx++) {
+            final String checkStr = newStr.substring(startIdx, startIdx + length);
+
+            if (isRightBracket(checkStr)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isOddLength(final int length) {
+        return length % 2 != 0;
+    }
+
+    private boolean isRightBracket(final String checkStr) {
+        final Deque<Character> stack = new ArrayDeque<>();
+        final char[] chars = checkStr.toCharArray();
+
+        for (final char ch : chars) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+                continue;
+            }
+
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            final char pop = stack.pop();
+
+            if (ch == ')' && pop != '('
+                    || ch == '}' && pop != '{'
+                    || ch == ']' && pop != '[') {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
