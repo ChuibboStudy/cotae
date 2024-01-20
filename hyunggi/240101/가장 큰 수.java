de80161775dabc3e9c6cbf41144d7a7378901798
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-01-20 14:34:27 +0900

    Hyunggi - 240101 문제풀이 (#20)
    * feat: 문자열 내 마음대로 정렬하기 upload
    
    * feat: 가장 큰 수 upload

---
+++ hyunggi/240101/가장 큰 수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String solution(final int[] numbers) {
+        final Comparator<String> comp = (o1, o2) -> (o2 + o1).compareTo(o1 + o2);
+
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted(comp)
+                .reduce((s1, s2) -> s1 + s2)
+                .filter(s -> !s.startsWith("0"))
+                .orElse("0");
+    }
+}
