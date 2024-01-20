a2aaec075647a7db6df4503e8801d8508f5c0dcf
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-01-20 14:35:02 +0900

    Hyunggi - 240106 문제풀이 (#23)
    * feat: 평행 upload
    
    * feat: 중복된 문자 제거 upload
    
    * feat: 중복된 문자 제거 - 리팩터링 upload

---
+++ hyunggi/240106/중복된 문자 제거.java
@@ -0,0 +1,10 @@
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(final String my_string) {
+        return my_string.chars()
+                .distinct()
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+    }
+}
