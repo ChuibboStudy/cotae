f01175a945139620bfa3870d1bc55b17d27eab90
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-01-20 14:35:45 +0900

    Hyunggi - 240113 문제풀이 (#27)
    * feat: 사칙연산 upload
    
    * feat: 올바른 괄호 upload

---
+++ hyunggi/240113/올바른 괄호.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    boolean solution(final String s) {
+        final Deque<Character> deque = new ArrayDeque<>();
+
+        final char[] charArr = s.toCharArray();
+
+        for (final char ch : charArr) {
+            if (ch == '(') {
+                deque.push(ch);
+                continue;
+            }
+
+            if (deque.isEmpty()) {
+                return false;
+            }
+
+            final char top = deque.peek();
+            if (top == ')') {
+                return false;
+            }
+
+            deque.pop();
+        }
+
+        return deque.isEmpty();
+    }
+}
