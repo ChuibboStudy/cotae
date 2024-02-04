8434a3a8b8370e45be7bbbb1909bd610c955abf4
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-02-04 18:20:38 +0900

    Hyunggi - 230130 문제풀이 (#35)
    * feat: 키패드 누르기 upload
    
    * feat: 타겟 넘버 upload

---
+++ hyunggi/240130/타겟 넘버.java
@@ -0,0 +1,29 @@
+/*
+아이디어 - DFS
+ */
+class Solution {
+    int gTarget;
+    int answer;
+
+    public int solution(final int[] numbers, final int target) {
+        gTarget = target;
+        answer = 0;
+
+        dfs(numbers, 0, 0);
+
+        return answer;
+    }
+
+    private void dfs(final int[] numbers, final int index, final int sum) {
+        if (index == numbers.length) {
+            if (sum == gTarget) {
+                answer++;
+            }
+
+            return;
+        }
+
+        dfs(numbers, index + 1, sum + numbers[index]);
+        dfs(numbers, index + 1, sum - numbers[index]);
+    }
+}
