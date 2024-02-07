40b1d6e892a91adef67a38dfcc7925931ad4f835
HyunggiPark <phg9587@naver.com>
2024-02-07 11:52:42 +0900

    feat: k진수에서 소수 개수 구하기 upload

---
+++ hyunggi/240207/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,38 @@
+
+class Solution {
+    public int solution(final int n, final int k) {
+        int answer = 0;
+
+        //k진수에 맞춰서 n을 변환
+        final String convert = Integer.toUnsignedString(n, k);
+
+        //0으로 split
+        final String[] split = convert.split("0");
+
+        //해당 값이 소수인지 판단
+        for (final String s : split) {
+            if (isPrimeNum(s)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isPrimeNum(final String s) {
+        if (s.isBlank()) return false;
+
+        final long val = Long.parseLong(s);
+
+        if (val <= 1) return false;
+
+        final long root = (long) Math.sqrt(val);
+        for (int i = 2; i <= root; i++) {
+            if (val % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
