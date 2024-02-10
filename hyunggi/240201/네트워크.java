a46aa6939ddb346dcb84d486118a0d5144cc7d49
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-02-10 14:44:11 +0900

    Hyunggi - 230201 문제풀이 (#36)
    * feat: 네트워크 upload
    
    * feat: 단어변환 upload

---
+++ hyunggi/240201/네트워크.java
@@ -0,0 +1,34 @@
+/*
+아이디어 - DFS
+네트워크 개수 구하기
+ */
+
+class Solution {
+    public int solution(final int n, final int[][] computers) {
+        int answer = 0;
+        final boolean[] isVisited = new boolean[n];
+
+        //네트워크 파악을 위해 0 ~ n-1 까지 순회
+        for (int start = 0; start < n; start++) {
+            if (isVisited[start]) continue;
+
+            dfs(computers, isVisited, start);
+            answer++;
+        }
+
+        return answer;
+    }
+
+    private void dfs(final int[][] computers, final boolean[] isVisited, final int start) {
+        isVisited[start] = true;
+        final int[] computer = computers[start];
+
+        for (int next = 0; next < computer.length; next++) {
+            final int isAble = computer[next];
+            if (isVisited[next] || isAble == 0) continue;
+
+            isVisited[next] = true;
+            dfs(computers, isVisited, next);
+        }
+    }
+}
---
+++ hyunggi/240201/단어변환.java
@@ -0,0 +1,58 @@
+/*
+아이디어 - DFS / BFS
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    private int answer;
+    private String gTarget;
+
+    public int solution(final String begin, final String target, final String[] words) {
+        answer = Integer.MAX_VALUE;
+        gTarget = target;
+
+        final Set<String> isVisited = new HashSet<>();
+        dfs(words, begin, 0, isVisited);
+
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+
+    void dfs(final String[] words, final String curWord, final int count, final Set<String> isVisited) {
+        if (curWord.equals(gTarget)) {
+            answer = Math.min(count, answer);
+            return;
+        }
+
+        for (final String word : words) {
+            if (!isDiffOneChar(curWord, word) || isVisited.contains(word)) continue;
+
+            isVisited.add(word);
+            dfs(words, word, count + 1, isVisited);
+            isVisited.remove(word);
+        }
+    }
+
+    private boolean isDiffOneChar(final String curWord, final String nextWord) {
+        if (curWord.length() != nextWord.length()) {
+            return false;
+        }
+
+        final int len = curWord.length();
+
+        int diffCount = 0;
+        for (int i = 0; i < len; i++) {
+            if (curWord.charAt(i) != nextWord.charAt(i)) {
+                diffCount++;
+            }
+
+            if (diffCount > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
