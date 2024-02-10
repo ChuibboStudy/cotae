83d56a0a97d7ee5a6069b546a8019220def98bdc
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-02-10 14:44:32 +0900

    Hyunggi - 230205 문제풀이 (#38)
    * feat: 큰 수 만들기 upload
    
    * feat: 단속 카메라 upload

---
+++ hyunggi/240205/큰 수 만들기.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Solution {
+    public String solution(final String number, int k) {
+        final Stack<Integer> stack = new Stack<>();
+
+        final int[] nums = getNums(number);
+
+        for (final int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num && 0 < k) {
+                stack.pop();
+                k--;
+            }
+
+            stack.push(num);
+        }
+
+        for (int i = k; i > 0; i--) {
+            stack.pop();
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        for (final Integer val : stack) {
+            sb.append(val);
+        }
+
+        return sb.toString();
+    }
+
+    private int[] getNums(final String number) {
+        final char[] charArray = number.toCharArray();
+        final int[] tempNums = new int[number.length()];
+
+        for (int i = 0; i < charArray.length; i++) {
+            tempNums[i] = charArray[i] - '0';
+        }
+
+        return tempNums;
+    }
+}
