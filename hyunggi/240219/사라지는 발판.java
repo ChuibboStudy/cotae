6f81cc0d9f159eb2c69aaa75103cd0fce97fadc9
Hyunggi <80137359+Denia-park@users.noreply.github.com>
2024-02-19 22:34:52 +0900

    Hyunggi - 230219 문제풀이 (#41)
    * feat: 양과 늑대 upload
    
    * feat: 파괴되지 않은 건물 upload
    
    * feat: 사라지는 발판 upload

---
+++ hyunggi/240219/사라지는 발판.java
@@ -0,0 +1,67 @@
+
+class Solution {
+    // 방향 벡터 정의: 상, 하, 좌, 우
+    private static final int[] dr = {-1, 1, 0, 0};
+    private static final int[] dc = {0, 0, -1, 1};
+    // '무한대'를 나타내는 상수, 최소 턴 계산에 사용
+    private static final int INF = 987654321;
+
+    // 주요 해결 방법을 구현한 메서드
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        // 두 플레이어 위치를 기반으로 최소 턴 수를 계산
+        return solve(board, aloc[0], aloc[1], bloc[0], bloc[1])[1];
+    }
+
+    // 게임의 승패와 최소 턴 수를 계산하는 메서드
+    private int[] solve(int[][] board, int r1, int c1, int r2, int c2) {
+        // 현재 위치에서 게임이 끝났는지 확인
+        if (isFinished(board, r1, c1)) return new int[]{0, 0};
+
+        // 두 플레이어가 같은 위치에 있으면 현재 플레이어 승리
+        if (r1 == r2 && c1 == c2) return new int[]{1, 1};
+
+        int minTurn = INF, maxTurn = 0;
+        boolean canWin = false;
+
+        // 모든 가능한 이동 시도
+        for (int i = 0; i < 4; i++) {
+            int nr = r1 + dr[i];
+            int nc = c1 + dc[i];
+            if (!inRange(board, nr, nc) || board[nr][nc] == 0) continue; // 이동할 수 없는 경우
+
+            board[r1][c1] = 0; // 현재 위치에서 이동
+            int[] result = solve(board, r2, c2, nr, nc); // 재귀적으로 다음 턴 계산
+            board[r1][c1] = 1; // Undo move
+
+            // 승리할 수 있는 경우와 그렇지 않은 경우를 분리하여 처리
+            if (result[0] == 0) { //상대방이 패배한다는 의미 -> 내가 승리
+                canWin = true;
+                minTurn = Math.min(minTurn, result[1]);
+            } else if (!canWin) {
+                maxTurn = Math.max(maxTurn, result[1]);
+            }
+        }
+
+        // 승리할 수 있는 경우 최소 턴, 그렇지 않은 경우 최대 턴 반환
+        int turn = canWin ? minTurn : maxTurn;
+        // 승패를 판단하고, 현재 턴을 소모하므로 turn + 1을 반환
+        return new int[]{canWin ? 1 : 0, turn + 1};
+    }
+
+    // 현재 위치에서 더 이동할 수 있는지 확인하는 메서드
+    private boolean isFinished(int[][] board, int r, int c) {
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (inRange(board, nr, nc) && board[nr][nc] == 1) {
+                return false; // 이동할 수 있는 위치가 있으면 게임 계속
+            }
+        }
+        return true; // 더 이동할 수 없으면 게임 종료
+    }
+
+    // 주어진 위치가 보드판 내에 있는지 확인하는 메서드
+    private boolean inRange(int[][] board, int r, int c) {
+        return r >= 0 && r < board.length && c >= 0 && c < board[0].length;
+    }
+}
---
+++ hyunggi/240219/파괴되지 않은 건물.java
@@ -0,0 +1,64 @@
+//https://school.programmers.co.kr/questions/25471
+
+class Solution {
+    static final int ATTACK = 1;
+    static final int HEAL = 2;
+
+    public int solution(final int[][] board, final int[][] skill) {
+        int answer = 0;
+
+        int[][] newBoard = new int[board.length + 1][board[0].length + 1];
+
+        //누적합 구하기
+        //(r1, c1) ~ (r2, c2) 좌표에 n의 값을 변화주고 싶다.
+        //r1, c1 = n
+        //r1, c2 + 1 = -n
+        //r2 + 1, c1 = -n
+        //r2 + 1 , c2 + 1 = n
+
+        //오른쪽으로 다 더하고 아래쪽으로 더하기
+
+        //skill을 다 적용
+        for (final int[] ints : skill) {
+            final int type = ints[0] == ATTACK ? -1 : 1;
+            final int r1 = ints[1];
+            final int c1 = ints[2];
+            final int r2 = ints[3];
+            final int c2 = ints[4];
+            final int degree = ints[5];
+
+            final int add = type * degree;
+
+            newBoard[r1][c1] += add;
+            newBoard[r1][c2 + 1] -= add;
+            newBoard[r2 + 1][c1] -= add;
+            newBoard[r2 + 1][c2 + 1] += add;
+        }
+
+        //누적합 계산
+        for (int r = 0; r < newBoard.length; r++) {
+            for (int c = 1; c < newBoard[0].length; c++) {
+                newBoard[r][c] += newBoard[r][c - 1];
+            }
+        }
+
+        for (int c = 0; c < newBoard[0].length; c++) {
+            for (int r = 1; r < newBoard.length; r++) {
+                newBoard[r][c] += newBoard[r - 1][c];
+            }
+        }
+
+        //멀쩡한 건물의 수를 센다.
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                int sum = newBoard[r][c] + board[r][c];
+
+                if (sum > 0) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
